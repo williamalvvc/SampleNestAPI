c3e2107e61307911895168e5d517bd5f52407ed1
William Alvarado <williamalvvv@Williams-MacBook-Pro.local>
2022-01-18 22:37:35 -0600

    🐛 Fixing expected values for running Unit Tests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get()
+  getSalute(): string {
+    return this.appService.getSalute();
+  }
 }
